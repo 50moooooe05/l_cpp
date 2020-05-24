b8caaafc32a9bd2e0f13252679654c7c414563a0
西野萌 <moe@Ms-MacBook.local>
2020-05-24 22:32:00 +0900

    影がついた

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <cmath>
 #include "vec3.h"
@@ -11,15 +12,18 @@ int main(){
 
     Sphere sphere(Vec3(0,0,0),1.0);
 
-    for(int  i = 0; i < 512; i++){
-        for(int j = 0; j < 512; j++){
+    Vec3 lightDir = normalize(Vec3(1,1,-1)); //光源の方向
+
+    for(int  i = 0; i < img.width; i++){
+        for(int j = 0; j < img.height; j++){
             double u = (2.0 * i - img.width) / img.width;
             double v = (2.0 * j - img.height) / img.height;
             Ray ray = cam.getRay(u,v);
             
             Hit hit;
             if(sphere.intersect(ray,hit)){
-                img.setPixel(i,j,(hit.hitNormal + Vec3(1,1,1))/2.0);
+                double I = std::max(dot(lightDir,hit.hitNormal),0.0);
+                img.setPixel(i,j,I * Vec3(1,1,1));
             }else{
                 img.setPixel(i,j,Vec3(0,0,0));
             }
